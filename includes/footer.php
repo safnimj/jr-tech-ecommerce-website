273ae60c528b9497df01586d13a5a663e522399d
SAFNI mj <mjsafnimax@gmail.com>
2024-10-21 21:51:48 +0530

    first-commit

---
+++ includes/footer.php
@@ -0,0 +1,100 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>JR Tech</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+    <div class="module-body m-t-20">
+        <p class="about-us"> At JR Tech, we bring you the latest and greatest in technology. From smartphones to laptops, home appliances to gaming consoles, we have everything you need to stay connected, entertained, and productive. Explore a wide range of high-quality products from top brands, all at unbeatable prices. Whether you're a tech enthusiast or just looking for the best deals, JR Tech is here to deliver innovation right to your doorstep.
+
+</p>
+
+    </div>
+
+</div>          	</div>
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening time</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Monday-Friday:</td><td class="pull-right">09.00 To 21.00</td></tr>
+					<tr><td>Saturday:</td><td class="pull-right">09.00 To 20.00</td></tr>
+					<tr><td>Sunday:</td><td class="pull-right">Closed</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">information</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Munni Street, Batticloa, Srilanka</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(065) 222 6630<br>(077) 7879865</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="#">mail@jrtech.lk</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
